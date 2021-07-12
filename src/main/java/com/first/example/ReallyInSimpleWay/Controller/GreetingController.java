e841d8cd49d2c79215698107d0d3db1d8abcacd4
Usha Chauhan <ushachauhan1992@gmail.com>
2021-07-12 17:50:40 +0530

    next commit

--- src/main/java/com/first/example/ReallyInSimpleWay/Controller/GreetingController.java
+++ src/main/java/com/first/example/ReallyInSimpleWay/Controller/GreetingController.java
@@ -1,11 +1,10 @@
 package com.first.example.ReallyInSimpleWay.Controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class GreetingController {
 
 	@GetMapping("/information")
